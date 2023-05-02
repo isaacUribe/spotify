950a8a6985a8a59da2e78b17914049381961ec5a
isaacUribe <iuribear@cesde.net>
2023-05-02 10:53:10 -0500

    se terminar las tarjetas con las canciones

--- src/controlador.js
+++ src/controlador.js
@@ -11,23 +11,36 @@ pedirTokenEnSpotify().then(function(token){
             columna.classList.add("col")
 
             let tarjeta = document.createElement("div")
+            tarjeta.id = "tarjeta"
             tarjeta.classList.add("card", "h-100", "shadow")
 
+            let titulo = document.createElement("h4")
+            titulo.classList.add('card-title')
+            let name = cancion.name
+            titulo.innerHTML = name
+            // console.log(cancion.name)
+
+            let imagen = document.createElement("img")
+            imagen.classList.add("card-img-top")
+            imagen.src = cancion.album.images[1].url
 
             let pista = document.createElement("audio")
+            pista.id = "pista"
             pista.classList.add("w-100")
             pista.setAttribute("controls", "controls")
             pista.src=cancion.preview_url
 
+            tarjeta.appendChild(imagen)
+            tarjeta.appendChild(titulo)
             tarjeta.appendChild(pista)
             columna.appendChild(tarjeta)
             fila.appendChild(columna)
         })
 
-        console.log(respuesta.tracks[1])
-        console.log(respuesta.tracks[1].name)
-        console.log(respuesta.tracks[1].preview_url)
-        console.log(respuesta.tracks[1].album.images[0].url)
+        // console.log(respuesta.tracks[1])
+        // console.log(respuesta.tracks[1].name)
+        // console.log(respuesta.tracks[1].preview_url)
+        // console.log(respuesta.tracks[1].album.images[0].url)
     })
 })
 
